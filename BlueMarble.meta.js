0832bd6e689790b3a33106588de7e83b6777aedd
Jonas Kah <asango@uni-koblenz.de>
2025-09-26 16:55:53 +0200

    added distribution files

---
+++ BlueMarble.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Blau Murmel
+// @namespace    https://github.com/asango100/Wplace-BlueMarble-MinorMods
+// @version      0.1.0
+// @description  A userscript to enhance Wplace.live. Forked from BlueMarble.
+// @match        https://wplace.live/*
+// @run-at       document-end
+// @updateURL    https://cdn.jsdelivr.net/gh/asango100/Wplace-BlueMarble-MinorMods@main/BlueMarble.meta.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/asango100/Wplace-BlueMarble-MinorMods@main/BlueMarble.user.js
+// ==/UserScript==
